f76009638608239002af297e887959070b701445
aknackd <adam@pyknotic.net>
2016-06-28 21:31:15 -0500

    Added rbenv install script

---
+++ scripts/setup-rbenv.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+#
+# Installs and sets up rbenv - Ruby Version Manager
+#
+# This script *does not* do the following:
+#
+#     1. Add `$RBENV_ROOT/bin` to your PATH
+#     2. Add `eval "$(rbenv init -)"` to your shell rc
+#
+# You must do these things manually yourself.
+#
+
+set -o errexit
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+readonly DIR="$( cd -P "$( dirname "$SOURCE" )/.." && pwd )"
+
+cd "$DIR"
+
+source "${DIR}/scripts/.common.sh"
+
+export RBENV_ROOT=$HOME/.rbenv
+export PATH="$PATH:$RBENV_ROOT/bin"
+
+# Install the latest stable version of rbenv
+install() {
+    log::info "===> Installing rbenv..."
+
+    git clone https://github.com/rbenv/rbenv.git "$RBENV_ROOT"
+
+    # try compiling the dynamic bash extension to speed up rbenv
+    set +o errexit
+    pushd "$RBENV_ROOT" >/dev/null ; src/configure ; make -C src ; popd >/dev/null
+
+    set -o errexit
+    eval "$(rbenv init -)"
+    log::info "---> rbenv version: $(rbenv --version | awk '{print $2}')"
+
+    # install plugins
+    local readonly plugins=(
+        rbenv/ruby-build
+        jf/rbenv-gemset
+    )
+
+    for plugin in "${plugins[@]}"; do
+        if ! test -d "$RBENV_ROOT/plugins/$(basename $plugin)"; then
+            log::info "---> Installing plugin: $plugin"
+            git clone https://github.com/${plugin}.git "$RBENV_ROOT/plugins/$(basename $plugin)"
+        fi
+    done
+}
+
+[ ! -f "$PYENV_ROOT/bin/rbenv" ] && install || log::warning "rbenv already installed! skipping"
