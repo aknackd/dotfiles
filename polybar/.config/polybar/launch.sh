1b83a96f15faeb7e51c990f4e1810e2cdf403199
aknackd <adam@pyknotic.net>
2020-09-15 19:53:21 -0500

    polybar: Rename example to topbar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -5,6 +5,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 for m in $(xrandr --query | grep ' connected' | cut -d' ' -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar --reload topbar &
 done
 
