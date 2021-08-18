095c9d1a8d497c0d55230fcbd77d4e25fce2c096
aknackd <adam@pyknotic.net>
2021-08-18 16:31:46 -0500

    neovim: Go to definitions in a separate tab

--- neovim/.config/nvim/lua/aknackd/lsp.lua
+++ neovim/.config/nvim/lua/aknackd/lsp.lua
@@ -24,3 +24,38 @@ local servers = {
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup { on_attach = on_attach }
 end
+
+-- Go to definitions in a split window
+-- Taken from https://github.com/neovim/nvim-lspconfig/wiki/UI-customization#go-to-definition-in-a-split-window
+local function goto_definition(split_cmd)
+    local util = vim.lsp.util
+    local log = require("vim.lsp.log")
+    local api = vim.api
+
+    local handler = function(_, method, result)
+        if result == nil or vim.tbl_isempty(result) then
+            local _ = log.info() and log.info(method, "No location found")
+            return nil
+        end
+
+        if split_cmd then
+            vim.cmd(split_cmd)
+        end
+
+        if vim.tbl_islist(result) then
+            util.jump_to_location(result[1])
+
+            if #result > 1 then
+                util.set_qflist(util.locations_to_items(result))
+                api.nvim_command("copen")
+                api.nvim_command("wincmd p")
+            end
+        else
+            util.jump_to_location(result)
+        end
+    end
+
+    return handler
+end
+
+vim.lsp.handlers["textDocument/definition"] = goto_definition('tabnew')
