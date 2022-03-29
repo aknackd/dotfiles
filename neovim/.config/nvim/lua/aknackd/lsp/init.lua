8ab342e667660042a8073ce27f7beffb6ffa6242
aknackd <adam@pyknotic.net>
2022-03-28 20:51:30 -0500

    neovim: Disable inline text for showing diagnostics
    To show diagnostics, use the <Leader>e keybinding added in the previous
    commit

--- neovim/.config/nvim/lua/aknackd/lsp/init.lua
+++ neovim/.config/nvim/lua/aknackd/lsp/init.lua
@@ -5,3 +5,8 @@ require('symbols-outline').setup({
     highlight_hovered_item = true,
     show_guides = true,
 })
+
+-- Disable inline text for showing diagnostics, use the keybinding instead
+vim.diagnostic.config({
+    virtual_text = false,
+})
