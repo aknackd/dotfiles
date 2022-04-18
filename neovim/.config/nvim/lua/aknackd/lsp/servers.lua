c7c63c0563c2685324f7f044faaf442b2c651f7c
aknackd <adam@pyknotic.net>
2022-04-18 09:28:25 -0500

    neovim: Added volar lsp server

--- neovim/.config/nvim/lua/aknackd/lsp/servers.lua
+++ neovim/.config/nvim/lua/aknackd/lsp/servers.lua
@@ -12,6 +12,7 @@ local servers = {
     'bashls',        -- Install via `yarn global add bash-language-server`
     'intelephense',  -- Install via `yarn global add intelephense`
     'tsserver',      -- Install via `yarn global add typescript typescript-language-server`
+    'volar',         -- Install via `yarn global add @volar/vue-language-server`
  }
 
  -- Setup lspconfig
