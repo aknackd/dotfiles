a5411245beadf45f5286f82900904a803e71b3fd
aknackd <adam@pyknotic.net>
2023-04-06 16:41:11 -0500

    chore(neovim): Adjusted completion plugins configuration
    Grouped hrsh7th's cmp plugins to be explicit dependencies of nvim-cmp

--- neovim/.config/nvim/lua/user/plugins.lua
+++ neovim/.config/nvim/lua/user/plugins.lua
@@ -50,12 +50,17 @@ require('lazy').setup {
             { 'neovim/nvim-lspconfig' },
 
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-buffer' },
-            { 'hrsh7th/cmp-path' },
             { 'saadparwaiz1/cmp_luasnip' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'hrsh7th/cmp-nvim-lua' },
+            {
+                'hrsh7th/nvim-cmp',
+                event = 'InsertEnter',
+                dependencies = {
+                    { 'hrsh7th/cmp-buffer' },
+                    { 'hrsh7th/cmp-nvim-lsp' },
+                    { 'hrsh7th/cmp-nvim-lua' },
+                    { 'hrsh7th/cmp-path' },
+                },
+            },
 
             -- Snippets
             { 'L3MON4D3/LuaSnip' },
