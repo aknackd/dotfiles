15d41ef22468f4f94622fa6a3fdbdc51039b72cf
aknackd <adam@pyknotic.net>
2023-08-02 14:11:02 -0500

    feat(neovim): Autoformat Go files on save
    Works via sbdchd/neoformat which calls out to gofmt.

--- neovim/.config/nvim/lua/user/plugins/neoformat.lua
+++ neovim/.config/nvim/lua/user/plugins/neoformat.lua
@@ -3,6 +3,6 @@ vim.g['neoformat_try_node_exe'] = 1
 vim.api.nvim_create_augroup("neoformat_buf", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePre", {
 	group   = "neoformat_buf",
-	pattern = { "*.js", "*.ts", "*.jsx", "*.tsx", "*.json" },
+	pattern = { "*.js", "*.ts", "*.jsx", "*.tsx", "*.json", "*.go" },
 	command = "Neoformat",
 })
