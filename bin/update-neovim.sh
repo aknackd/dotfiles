aad0fc1ef25e4ab8c85cd68959f322f83693cfb1
aknackd <adam@pyknotic.net>
2023-08-16 18:44:12 -0500

    feat(update-neovim.sh): Specify options via flags
    In addition to specifying options via environment variables, options can
    be specified by flags. Usage:
    
        -b, --branch=BRANCH        Specify the neovim branch to build
        -c, --num-commits=NUM      Specify the last NUM commits to show after fetching neovim updates
        -j, --jobs=NUM             Allow NUM jobs at once
        -n, --nice=NUM             Specify nice adjustment
        -p, --prefix=PATH          Directory prefix where neovim will be installed
        -s, --source=PATH          Directory path where neovim source is located
    
    If the command line flag is specified, that value is used instead of the
    environment variable (if present). If neither a command line flag *and*
    environment variable is specified, then a default value is used. The
    defaults are as follows:
    
        Branch:             master
        Build niceness:     +15
        Number of commits:  15
        Jobs:               <number of logical CPU cores from nproc/sysctl>
        Build prefix:       /usr/local/Cellar/neovim
        Source directory:   /usr/local/src/neovim

--- bin/update-neovim.sh
+++ bin/update-neovim.sh
@@ -10,7 +10,7 @@
 # /usr/local/Cellar/neovim) along with a symlink to the build named "latest" in
 # the same build directory (similar to how Homebrew works).
 #
-# Makes the following assumptions:
+# This script makes the following assumptions:
 #
 #     1. All build dependencies already installed (see https://github.com/neovim/neovim/wiki/Building-Neovim#build-prerequisites)
 #     2. The neovim git repo has already been cloned
@@ -18,40 +18,91 @@
 #     4. The install directory has r/w access for the user running the script
 #     5. A symlink to "$NVIM_PREFIX/latest" exists somewhere in your PATH (ex: /usr/local/bin/nvim)
 #
-# Environment variables for customization:
-#
-#     | Name             | Description                                              | Default                       |
-#     |:-----------------|:---------------------------------------------------------|:------------------------------|
-#     | NVIM_BRANCH      | Branch to build                                          | master                        |
-#     | NVIM_SOURCE_DIR  | Path to source directory                                 | $HOME/Workspace/neovim/neovim |
-#     | NVIM_PREFIX      | Directory prefix where neovim will be installed          | /usr/local/Cellar/neovim      |
-#     | NVIM_NUM_COMMITS | The last x commits to show after fetching latest commits | 15                            |
-#     | NVIM_NUM_JOBS    | The number of jobs (commands) to run simultaneously      | `nproc`                       |
-#
 
+SELF="$0"
+
+# Colors
 GREEN="$(echo -e "\033[0;32m")"
 YELLOW="$(echo -e "\033[0;33m")"
 RESET="$(echo -e "\033[0;0m")"
+
+# Defaults
+DEFAULT_NVIM_BRANCH="master"
+DEFAULT_NVIM_NICENESS="+15"
+DEFAULT_NVIM_NUM_COMMITS=15
+DEFAULT_NVIM_NUM_JOBS="$(nproc 2>/dev/null || sysctl -n hw.ncpu)"
+DEFAULT_NVIM_PREFIX="/usr/local/Cellar/neovim"
+DEFAULT_NVIM_SOURCE_DIR="/usr/local/src/neovim"
 
 function log() {
     printf "[%s%s%s] %s%s%s\n" "$YELLOW" "$(date +'%Y-%m-%dT%H:%M:%S')" "$RESET" "$GREEN" "$1" "$RESET"
 }
 
-NVIM_BRANCH="${NVIM_BRANCH:-master}"
-NVIM_SOURCE_DIR="${NVIM_SOURCE_DIR:-${HOME}/Workspace/neovim/neovim}"
-NVIM_PREFIX="${NVIM_PREFIX:-/usr/local/Cellar/neovim}"
-NVIM_NUM_COMMITS="${NVIM_NUM_COMMITS:-15}"
-NVIM_NUM_JOBS="${NVIM_NUM_JOBS:-$(nproc 2>/dev/null || sysctl -n hw.ncpu)}"
+function error_log() {
+    printf "[%s%s%s] %s%s%s\n" "$YELLOW" "$(date +'%Y-%m-%dT%H:%M:%S')" "$RESET" "$GREEN" "$1" "$RESET" 1>&2
+}
+
+function usage() {
+    1>&2 printf "%s [OPTIONS]\n\n" "$(basename "$SELF")"
+    1>&2 printf "Builds Neovim from source\n\n"
+    1>&2 printf "OPTIONS\n"
+    1>&2 printf "  -b, --branch=BRANCH        Specify the neovim branch to build\n"
+    1>&2 printf "  -c, --num-commits=NUM      Specify the last NUM commits to show after fetching neovim updates\n"
+    1>&2 printf "  -j, --jobs=NUM             Allow NUM jobs at once (default: %d)\n" "$DEFAULT_NVIM_NUM_JOBS"
+    1>&2 printf "  -n, --nice=NUM             Specify nice adjustment (default: %s)\n" "$DEFAULT_NVIM_NICENESS"
+    1>&2 printf "  -p, --prefix=PATH          Directory prefix where neovim will be installed (default: %s)\n" "$DEFAULT_NVIM_PREFIX"
+    1>&2 printf "  -s, --source=PATH          Directory path where neovim source is located (default: %s)\n" "$DEFAULT_NVIM_SOURCE_DIR"
+    1>&2 printf "      --help                 Display this help text and exit\n"
+    exit 1
+}
+
+ARGS=$(getopt -o b:c:j:n:p:s: --long branch:,num-commits:,nice:,jobs:,prefix:,source:,help -- "$@")
+eval set -- "$ARGS"
+
+while [ : ]; do
+    case "$1" in
+        -b | --branch)      NVIM_BRANCH="$2"      ; shift 2 ;;
+        -c | --num-commits) NVIM_NUM_COMMITS="$2" ; shift 2 ;;
+        -j | --jobs)        NVIM_NUM_JOBS="$2"    ; shift 2 ;;
+        -n | --nice)        NVIM_NICENESS="$2"    ; shift 2 ;;
+        -p | --prefix)      NVIM_PREFIX="$2"      ; shift 2 ;;
+        -s | --source)      NVIM_SOURCE_DIR="$2"  ; shift 2 ;;
+        --help)             usage ;;
+        --) shift ; break ;;
+    esac
+done
+
+NVIM_BRANCH="${NVIM_BRANCH:-${NVIM_BRANCH:-${DEFAULT_NVIM_BRANCH}}}"
+NVIM_SOURCE_DIR="${NVIM_SOURCE_DIR:-${NVIM_SOURCE_DIR:-${DEFAULT_NVIM_SOURCE_DIR}}}"
+NVIM_PREFIX="${NVIM_PREFIX:-${NVIM_PREFIX:-${DEFAULT_NVIM_PREFIX}}}"
+# TODO: Parse to int
+NVIM_NUM_COMMITS="${NVIM_NUM_COMMITS:-${NVIM_NUM_COMMITS:-${DEFAULT_NVIM_NUM_COMMITS}}}"
+# TODO: Parse to int
+NVIM_NICENESS="${NVIM_NICENESS:-${NVIM_NICENESS:-${DEFAULT_NVIM_NICENESS}}}"
+# TODO: Parse to int
+NVIM_NUM_JOBS="${NVIM_NUM_JOBS:-${NVIM_NUM_JOBS:-${DEFAULT_NVIM_NUM_JOBS}}}"
 
 make_cmd="make"
 if [[ "$(uname -s)" == "FreeBSD" ]]; then
     make_cmd="gmake"
 fi
 
+# TODO: Clone neovim if $NVIM_SOURCE_DIR doesn't exist
+
 if [[ ! -d "$NVIM_SOURCE_DIR" ]]; then
     log "Unable to find source directory $NVIM_SOURCE_DIR; exiting"
     exit 1
 fi
+
+log "Building neovim with the following configuration:"
+1>&2 printf "\n"
+1>&2 printf "    Build niceness:                        %s\n" "$NVIM_NICENESS"
+1>&2 printf "    Build prefix:                          %s\n" "$NVIM_PREFIX"
+1>&2 printf "    Neovim source branch:                  %s\n" "$NVIM_BRANCH"
+1>&2 printf "    Neovim source directory:               %s\n" "$NVIM_SOURCE_DIR"
+1>&2 printf "    Number of parallel build jobs:         %d\n" "$NVIM_NUM_JOBS"
+1>&2 printf "    Number of recent commits to display:   %d\n" "$NVIM_NUM_COMMITS"
+1>&2 printf "\n"
 
 cd "$NVIM_SOURCE_DIR"
 
@@ -73,7 +124,7 @@ if [[ -f "${NVIM_PREFIX}/${COMMIT}/bin/nvim" ]]; then
 fi
 
 log "Building from commit ${COMMIT} ..."
-nice -n +15 "$make_cmd" -j "$NVIM_NUM_JOBS" install
+nice -n "$NVIM_NICENESS" "$make_cmd" -j "$NVIM_NUM_JOBS" install
 
 cd "$NVIM_PREFIX"
 
@@ -83,8 +134,13 @@ ln -nfs "$COMMIT" latest
 log "Removing old neovim installs ..."
 find . -maxdepth 1 -type d \( ! -name "$COMMIT" -a ! -name . \) -exec rm -rf {} \;
 
-log "Printing newly built neovim version ..."
-nvim --version | head -n 1
+if which nvim 2>/dev/null ; then
+    log "Printing newly built neovim version ..."
+    nvim --version | head -n 1
+else
+    error_log "nvim not found in \$PATH"
+    error_log "Be sure to setup a symlink somewhere in your PATH that links to ${NVIM_PREFIX}/latest"
+    exit 1
+fi
 
 log "Done!"
-log "Be sure to setup a symlink somewhere in your PATH that links to ${NVIM_PREFIX}/latest"
