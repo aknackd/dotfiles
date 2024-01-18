c8e33237e8480b49fbc4657d5b07121400a051db
aknackd <adam@pyknotic.net>
2024-01-18 09:32:08 -0600

    feat(neovim): Allow specifying filetypes to autoformat on save with Neoformat
    If the NVIM_NEOFORMAT_ON_SAVE environment variable is defined with a
    comma separated list of filetypes, those filetypes will be automatically
    formatted on save using Neoformat. If the environment variable is not
    defined then the following default list of filetypes will be used:
    
        - *.js
        - *.ts
        - *.jsx
        - *.tsx
        - *.json
        - *.go
        - *.py

--- neovim/.config/nvim/lua/user/plugins/neoformat.lua
+++ neovim/.config/nvim/lua/user/plugins/neoformat.lua
@@ -1,8 +1,12 @@
+local split = require('user.utils').split
+
 vim.g['neoformat_try_node_exe'] = 1
+
+local default_autoformat_fts = "*.js,*.ts,*.jsx,*.tsx,*.json,*.go,*.py"
 
 vim.api.nvim_create_augroup("neoformat_buf", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePre", {
 	group   = "neoformat_buf",
-	pattern = { "*.js", "*.ts", "*.jsx", "*.tsx", "*.json", "*.go", "*.py" },
+	pattern = split(",", os.getenv("NVIM_NEOFORMAT_ON_SAVE") or default_autoformat_fts),
 	command = "Neoformat",
 })
