0062f737b144fd5972fe29225aba6754127096c7
aknackd <adam@pyknotic.net>
2024-02-01 09:35:22 -0600

    chore(neovim): Formatting

--- neovim/.config/nvim/lua/user/plugins/neoformat.lua
+++ neovim/.config/nvim/lua/user/plugins/neoformat.lua
@@ -2,11 +2,12 @@ local split = require('user.utils').split
 
 vim.g['neoformat_try_node_exe'] = 1
 
-local default_autoformat_fts = "*.js,*.ts,*.jsx,*.tsx,*.json,*.go,*.py"
+local default_autoformat_filetypes = "*.js,*.ts,*.jsx,*.tsx,*.json,*.go,*.py"
+local autoformat_filetypes = split(",", os.getenv("NVIM_NEOFORMAT_ON_SAVE") or default_autoformat_filetypes)
 
 vim.api.nvim_create_augroup("neoformat_buf", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePre", {
 	group   = "neoformat_buf",
-	pattern = split(",", os.getenv("NVIM_NEOFORMAT_ON_SAVE") or default_autoformat_fts),
+	pattern = autoformat_filetypes,
 	command = "Neoformat",
 })
