93dc3dd8f28499fcee39aa1eaa4ecfed17c97262
aknackd <adam@pyknotic.net>
2024-10-12 05:54:46 -0500

    chore(install.sh): Remove neovim setup and skip Homebrew and Kitty themes installation by default
    We aren't using Packer for neovim packages anymore and the neovim config
    will automatically install Lazy and all configured packages on startup
    if Lazy isn't installed anyways.
    
    As well, don't attempt to install Homebrew or themes for Kitty unless
    the following environment variables are set:
    
        - INSTALL_HOMEBREW=y
        - INSTALL_KITTY_THEMES=y

--- install.sh
+++ install.sh
@@ -1,66 +1,64 @@
 #!/usr/bin/env bash
 
-declare -a DEPENDENCIES=( curl stow git tmux )
+declare -a DEPENDENCIES=(curl git tmux stow)
 
-readonly COLOR_RED="$(echo -e "\033[0;31m")"
-readonly COLOR_GREEN="$(echo -e "\033[0;32m")"
-readonly COLOR_BLUE="$(echo -e "\033[0;34m")"
-readonly COLOR_RESET="$(echo -e "\033[0;0m")"
+COLOR_RED="$(echo -e "\033[0;31m")"
+COLOR_GREEN="$(echo -e "\033[0;32m")"
+COLOR_BLUE="$(echo -e "\033[0;34m")"
+COLOR_RESET="$(echo -e "\033[0;0m")"
 
-function check::dependencies () {
+function check::dependencies() {
     for BIN in "${DEPENDENCIES[@]}"; do
-        if ! command -v "$BIN" >/dev/null ; then
+        if ! command -v "$BIN" >/dev/null; then
             >&2 echo "${COLOR_RED}ERROR${COLOR_RESET}: Please install ${COLOR_BLUE}${BIN}${COLOR_RESET} first before proceeding!"
             exit 1
         fi
     done
 }
 
-function install::homebrew () {
+function install::homebrew() {
     [[ $(uname -s) != "Darwin" ]] && return
 
-    if ! command -v brew >/dev/null 2>&1 ; then
+    if ! command -v brew >/dev/null 2>&1; then
         echo "${COLOR_GREEN}:: Installing homebrew ...${COLOR_RESET}"
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
         brew analytics off
     fi
 }
 
-function install::dotfiles () {
+function install::dotfiles() {
     echo "${COLOR_GREEN}:: Linking dotfiles ...${COLOR_RESET}"
     stow alacritty direnv git kitty neovim sqlite tmux vim wezterm zsh --target "$HOME" --verbose
 
-    mkdir -p "$HOME/.local/bin"
+    mkdir -pv "$HOME/.local/bin"
     stow bin --target "$HOME/.local/bin" --verbose
 
     # Setup ssh in home directory with the correct permissions to use
     # authorized_keys
-    mkdir -p "$HOME/.ssh/sessions" "$HOME/.ssh/conf.d/home" "$HOME/.ssh/conf.d/work"
-    # @@@ Don't update the timestamp if the file already exists
+    mkdir -pv "$HOME/.ssh/sessions" "$HOME/.ssh/conf.d/{home,work}"
     touch "$HOME/.ssh/authorized_keys"
     chmod 0700 "$HOME/.ssh"
     chmod 0600 "$HOME/.ssh/authorized_keys"
 
     # And copy over the ssh config file(s) since some systems can have strict
     # permissions that won't allow symlinks to work
-    cp -p ssh/.ssh/config "$HOME/.ssh/config"
-    # @@@ Don't update the timestamp if the files already exist
-    touch "$HOME/.ssh/conf.d/home/config" "$HOME/.ssh/conf.d/work/config"
-    chmod 0600 "$HOME/.ssh/config" "$HOME/.ssh/conf.d/home/config" "$HOME/.ssh/conf.d/work/config"
+    cp -pv ssh/.ssh/config "$HOME/.ssh/config"
+    touch "$HOME/.ssh/conf.d/{home,work}/config"
+    chmod 0600 "$HOME/.ssh/config" "$HOME/.ssh/conf.d/{home,work}/config"
 
     case "$(uname -s)" in
-        Linux)
-            echo "${COLOR_GREEN}:: Linking linux-specific dotfiles ...${COLOR_RESET}"
-            stow herbstluftwm i3 polybar rofi xorg --target "$HOME" --verbose
-            ;;
-        Darwin)
-            echo "${COLOR_GREEN}:: Linking macOS-specific dotfiles ...${COLOR_RESET}"
-            stow macOS --target "$HOME" --verbose
-            ;;
+    Linux)
+        echo "${COLOR_GREEN}:: Linking linux-specific dotfiles ...${COLOR_RESET}"
+        stow herbstluftwm i3 polybar rofi xorg --target "$HOME" --verbose
+        ;;
+    Darwin)
+        echo "${COLOR_GREEN}:: Linking macOS-specific dotfiles ...${COLOR_RESET}"
+        stow macOS --target "$HOME" --verbose
+        ;;
     esac
 }
 
-function setup::fzf () {
+function setup::fzf() {
     if [ ! -d "$HOME/.fzf" ]; then
         echo "${COLOR_GREEN}:: Setting up fzf ...${COLOR_RESET}"
         git clone --depth=1 https://github.com/junegunn/fzf.git "$HOME/.fzf"
@@ -68,12 +66,7 @@ function setup::fzf () {
     fi
 }
 
-function setup::neovim () {
-    echo "${COLOR_GREEN}:: Setting up neovim ...${COLOR_RESET}"
-    nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-}
-
-function setup::tmux () {
+function setup::tmux() {
     local tpmdir="$HOME/.tmux/plugins/tpm"
 
     if [ ! -d "$tpmdir" ]; then
@@ -82,16 +75,16 @@ function setup::tmux () {
     fi
 }
 
-function setup::vim () {
+function setup::vim() {
     local confDir="$HOME/.config/vim"
 
     if [ ! -d "$confDir" ]; then
         echo "${COLOR_GREEN}:: Setting up vim ...${COLOR_RESET}"
-        mkdir -p "$confDir" "$confDir/backup" "$confDir/temp"
+        mkdir -pv "${confDir}/{backup,temp}"
     fi
 }
 
-function setup::kitty () {
+function install::kitty_themes() {
     local confDir="$HOME/.config/kitty/kitty-themes"
 
     if [ ! -d "$confDir" ]; then
@@ -102,9 +95,8 @@ function setup::kitty () {
 
 check::dependencies
 install::dotfiles
-[[ "${SKIP_HOMEBREW:-n}" == "y" ]] || install::homebrew
-[[ "${SKIP_FZF:-n}" == "y" ]]      || setup::fzf
-[[ "${SKIP_NEOVIM:-n}" == "y" ]]   || setup::neovim
-[[ "${SKIP_TMUX:-n}" == "y" ]]     || setup::tmux
-[[ "${SKIP_VIM:-n}" == "y" ]]      || setup::vim
-[[ "${SKIP_KITTY:-n}" == "y" ]]    || setup::kitty
+[[ "${SKIP_FZF:-n}" == "y" ]] || setup::fzf
+[[ "${SKIP_TMUX:-n}" == "y" ]] || setup::tmux
+[[ "${SKIP_VIM:-n}" == "y" ]] || setup::vim
+[[ "${INSTALL_HOMEBREW:-n}" == "y" ]] && install::homebrew
+[[ "${INSTALL_KITTY_THEMES:-n}" == "y" ]] && install::kitty_themes
