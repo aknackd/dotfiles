b7660c5d1031d6f0347b0f1819942d951ca8e41d
aknackd <adam@pyknotic.net>
2024-12-28 03:36:50 -0600

    feat: Added ghostty configuration
    To add an environment specific configuration, simply create a file at
    $XDG_CONFIG_HOME/ghostty/config.local

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ function install::homebrew() {
 
 function install::dotfiles() {
     echo "${COLOR_GREEN}:: Linking dotfiles ...${COLOR_RESET}"
-    stow alacritty direnv git kitty neovim sqlite tmux vim wezterm zsh --target "$HOME" --verbose
+    stow alacritty direnv ghostty git kitty neovim sqlite tmux vim wezterm zsh --target "$HOME" --verbose
 
     mkdir -pv "$HOME/.local/bin"
     stow bin --target "$HOME/.local/bin" --verbose
