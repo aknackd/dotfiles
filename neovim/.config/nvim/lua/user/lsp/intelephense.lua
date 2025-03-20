49f966273acb3e278e4a76870f8ec4717d1b6ab5
aknackd <adam@pyknotic.net>
2025-03-20 17:47:21 -0500

    feat(neovim): Configure intelephense license key
    Rather than creating a file at $HOME/intelephense/licence.txt, the
    license key can be specified via the INTELEPHENSE_LICENSE_KEY
    environment variable

--- neovim/.config/nvim/lua/user/lsp/intelephense.lua
+++ neovim/.config/nvim/lua/user/lsp/intelephense.lua
@@ -3,6 +3,7 @@ local utils = require("user.utils")
 return {
 	init_options = {
 		globalStoragePath = utils.get_lsp_cache_dir() .. "/intelephense",
+		licenceKey = os.getenv("INTELEPHENSE_LICENSE_KEY") or nil,
 		telemetry = {
 			enabled = false,
 		},
