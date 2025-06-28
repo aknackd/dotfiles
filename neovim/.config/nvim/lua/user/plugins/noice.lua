86a9801529e0d11ee5d08abc5a49ac12019f4218
aknackd <adam@pyknotic.net>
2025-06-28 05:38:13 -0500

    change(neovim): Hide more annoying notification messages

--- neovim/.config/nvim/lua/user/plugins/noice.lua
+++ neovim/.config/nvim/lua/user/plugins/noice.lua
@@ -1,30 +1,30 @@
 vim.opt.lazyredraw = false
 
-require('noice').setup({
+require("noice").setup({
 	routes = {
 		-- Move undo/redo messages to the "mini" view
 		{
 			filter = {
-				event = 'msg_show',
+				event = "msg_show",
 				any = {
-					{ find = '%d+L, %d+B' },
-					{ find = '; after #%d+' },
-					{ find = '; before #%d+' },
-					{ find = 'Already at oldest change' },
-					{ find = 'Already at newest change' },
+					{ find = "%d+L, %d+B" },
+					{ find = "; after #%d+" },
+					{ find = "; before #%d+" },
+					{ find = "Already at oldest change" },
+					{ find = "Already at newest change" },
 				},
 			},
-			view = 'mini',
+			view = "mini",
 		},
 		-- Don't show yank/paste/delete messages
 		{
 			filter = {
-				event = 'msg_show',
+				event = "msg_show",
 				any = {
-					{ find = '%d lines yanked' },
-					{ find = '%d fewer lines' },
-					{ find = '%d more lines' },
-					{ find = 'No lines in buffer' },
+					{ find = "%d lines yanked" },
+					{ find = "%d fewer lines" },
+					{ find = "%d more lines" },
+					{ find = "No lines in buffer" },
 				},
 			},
 			opts = { skip = true },
@@ -32,12 +32,23 @@ require('noice').setup({
 		{
 			filter = {
 				any = {
-					{ find = 'nil' },
-					{ find = 'No information available' },
+					{ find = "nil" },
+					{ find = "No information available" },
 					-- Messages emitted by jdtls
-					{ find = 'Starting Java Language Server' },
-					{ find = 'Ready' },
-					{ find = 'ServiceReady' },
+					{ find = "Starting Java Language Server" },
+					{ find = "OK" },
+					{ find = "Ready" },
+					{ find = "ServiceReady" },
+				},
+			},
+			opts = { skip = true },
+		},
+		-- Don't show annoying messages
+		{
+			filter = {
+				any = {
+					{ find = "client.supports_method is deprecated" },
+					{ find = "syntax/java.vim: " },
 				},
 			},
 			opts = { skip = true },
@@ -46,30 +57,30 @@ require('noice').setup({
 		-- https://github.com/LazyVim/LazyVim/discussions/1439#discussioncomment-6971647
 		{
 			filter = {
-				event = 'lsp',
-				kind  = 'progress',
-				find  = 'jdtls',
+				event = "lsp",
+				kind = "progress",
+				find = "jdtls",
 			},
 			opts = { skip = true },
 		},
 		-- Route any messages with more than 20 lines to a split
 		{
-			filter = { event = 'msg_show', min_height = 20 },
-			view = 'split',
+			filter = { event = "msg_show", min_height = 20 },
+			view = "split",
 		},
 	},
 	cmdline = {
 		enabled = true,
-		view = 'cmdline',
+		view = "cmdline",
 	},
 	lsp = {
 		progress = {
 			enabled = false,
 		},
 		override = {
-			['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-			['vim.lsp.util.stylize_markdown'] = true,
-			['cmp.entry.get_documentation'] = true,
+			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+			["vim.lsp.util.stylize_markdown"] = true,
+			["cmp.entry.get_documentation"] = true,
 		},
 	},
 	presets = {
