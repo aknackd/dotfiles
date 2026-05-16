9ac981670efe47efb6e83f7661e58c11cc8f71cd
aknackd <adam@pyknotic.net>
2026-05-15 20:52:33 -0500

    fix(neovim): remove lua from ensure_installed
    This didn't seem to work as neovim would throw an error on the first run
    anyway.

--- neovim/.config/nvim/lua/user/plugins/nvim-lspconfig.lua
+++ neovim/.config/nvim/lua/user/plugins/nvim-lspconfig.lua
@@ -106,7 +106,7 @@ require("mason").setup({
 })
 
 require("mason-tool-installer").setup({
-	ensure_installed = { "lua", "stylua" },
+	ensure_installed = { "stylua" },
 	auto_update = false,
 	run_on_start = true,
 	start_delay = 3000,
